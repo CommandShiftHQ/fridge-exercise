bf19ebe4f346f206671bcbbfb0d2c5458997efd0
Chris Peakin <christopher.peakin@googlemail.com>
2021-11-24 21:00:34 +0000

    updated function, passes all 3 test

--- fridge.js
+++ fridge.js
@@ -1,5 +1,19 @@
-function findIngredients (){
+const fridgeContent = [
+    {name: 'eggs',saladIngredient: false},
+    {name: 'lettuce',saladIngredient: true},
+    {name: 'tomatoes',saladIngredient: true},
+    {name: 'carrots',saladIngredient: true},
+    {name: 'milk',saladIngredient: false}
+];
 
-}
+const findIngredients = (fridgeContent) => {
+    const ingredients = [];
+    for(let i = 0; i < fridgeContent.length; i+=1){
+        if(fridgeContent[i].saladIngredient === true){
+            ingredients.push(fridgeContent[i].name);
+        }
+    }
+    return ingredients;
+};
 
 module.exports = findIngredients;
